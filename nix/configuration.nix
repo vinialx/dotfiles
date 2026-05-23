a8ea2cbd25e2b65dabeb7f2a684fdbbbc6d1add7
vinialx <vini.aloise.silva@gmail.com>
2026-05-22 23:28:09 -0300

    chore: update ghostty and nix config

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -1,58 +1,38 @@
-
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [ ./hardware-configuration.nix ];
 
-  # Enable boot plymouth.
   boot = {
-  plymouth = {
-    enable = true;
-    theme = "mac-style";
-    themePackages = [ pkgs.mac-style-plymouth ];
+    plymouth = {
+      enable = true;
+      theme = "mac-style";
+      themePackages = [ pkgs.mac-style-plymouth ];
+    };
+
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+    initrd.kernelModules = [ "nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" ];
+    kernelParams = [
+      "quiet"
+      "udev.log_level=3"
+      "systemd.show_status=auto"
+      "nvidia-drm.modeset=1"
+    ];
   };
 
-  consoleLogLevel = 3;
-  initrd.verbose = false;
-  kernelParams = [
-    "quiet"
-    "udev.log_level=3"
-    "systemd.show_status=auto"
-  ];
-};
- 
-  # Bootloader.
   boot.loader = {
     timeout = null;
-    systemd-boot = {
-      enable = true;
-    	};
+    systemd-boot.enable = true;
     efi.canTouchEfiVariables = true;
   };
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
+  networking.hostName = "nixos";
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "America/Sao_Paulo";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "pt_BR.UTF-8";
     LC_IDENTIFICATION = "pt_BR.UTF-8";
@@ -65,72 +45,67 @@
     LC_TIME = "pt_BR.UTF-8";
   };
 
-  # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  # Enable the GNOME Desktop Environment.
+  # Wayland + GDM
   services.displayManager.gdm.enable = true;
   services.displayManager.gdm.wayland = true;
   services.desktopManager.gnome.enable = true;
 
-# Enable OpenGL / Hardware Graphics Acceleration
   hardware.graphics = {
     enable = true;
-    enable32Bit = true; # Highly recommended for games/Steam
+    enable32Bit = true;
   };
 
-  # Load the proprietary NVIDIA driver
   services.xserver.videoDrivers = [ "nvidia" ];
 
   hardware.nvidia = {
-    # Modesetting is required for most modern Wayland and X11 compositors
-    modesetting.enable = true;
-
-    # Enable the NVIDIA settings menu
     nvidiaSettings = true;
-
-    # Use the stable proprietary driver (NVIDIA Linux x64 Driver)
     package = config.boot.kernelPackages.nvidiaPackages.stable;
+    powerManagement.enable = true;        # importante em notebook
+    powerManagement.finegrained = false;
+    modesetting.enable = true;
+    open = false;
 
-    # ADD THIS LINE: Enable the open-source GPU kernel modules (recommended for RTX 4060)
-    open = true; 
-  };  
+    prime = {
+      sync.enable = true;
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+  };
 
-  # Configure keymap in X11
+  # Variáveis que corrigem o lag no GNOME Wayland com NVIDIA
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME = "nvidia";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    GBM_BACKEND = "nvidia-drm";
+    __GL_GSYNC_ALLOWED = "0";
+    __GL_VRR_ALLOWED = "0";
+    WLR_NO_HARDWARE_CURSORS = "1";
+    NIXOS_OZONE_WL = "1";
+  };
+
   services.xserver.xkb = {
     layout = "us";
     variant = "";
   };
 
-  # Enable CUPS to print documents.
   services.printing.enable = true;
-
-  # Enable unfree software.
   nixpkgs.config.allowUnfree = true;
-
-  # Enable flatpaks.
   services.flatpak.enable = true;
-
-  # Enable git.
   programs.git.enable = true;
-
-  #Enable docker.
   virtualisation.docker.enable = true;
- 
-  # Enable zsh module
+
   programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
-
   programs.zsh.ohMyZsh = {
     enable = true;
     theme = "af-magic";
     plugins = ["git" "sudo" "docker"];
   };
 
-  # Enable tailscale service.
   services.tailscale.enable = true;
 
-  # Enable sound with pipewire.
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -138,46 +113,27 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable nix flakes and experimental commands.
   nix.settings.experimental-features = ["nix-command" "flakes"];
 
-  # Enable auto garbage collection.
   nix.gc = {
     automatic = true;
     dates = "weekly";
     options = "--delete-older-than 5d";
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.vinicius = {
     isNormalUser = true;
     description = "vinicius";
-    extraGroups = [ "networkmanager" "wheel" "docker"];
-    packages = with pkgs; [
-    #  thunderbird
-    ];
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    packages = with pkgs; [];
   };
 
-  # Install firefox.
   programs.firefox.enable = false;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
-    spotify
     brave
     anydesk
     remmina
@@ -198,10 +154,9 @@
     docker
     docker-compose
     dbeaver-bin
-    flameshot
+    gnome-tweaks
   ];
 
-  # Enable nix-ld.
   programs.nix-ld.enable = true;
   programs.nix-ld.libraries = with pkgs; [
     stdenv.cc.cc
@@ -209,31 +164,5 @@
     glib
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.11"; # Did you read the comment?
-
+  system.stateVersion = "25.11";
 }
