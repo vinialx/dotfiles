553fbe235f38a6b454cddf120488a179e7e47758
vinialx <vini.aloise.silva@gmail.com>
2026-06-16 12:16:09 -0300

    chore: add proton-vpn package

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -70,6 +70,7 @@
     WLR_NO_HARDWARE_CURSORS = "1";
     NIXOS_OZONE_WL = "1";
   };
+
   services.xserver.xkb = {
     layout = "us";
     variant = "";
@@ -151,6 +152,7 @@
     onlyoffice-desktopeditors
     nodejs
     iscc
+    proton-vpn-cli
   ];
   
   programs.nix-ld.enable = true;
